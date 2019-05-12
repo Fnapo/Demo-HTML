d100cb4f3366c6180a57dfc475844f512bf7581b
Fnapo <36490842+Fnapo@users.noreply.github.com>
2019-05-12 23:39:48 +0200

    Creacion de un reloj SVG

--- script/relojes.js
+++ script/relojes.js
@@ -10,8 +10,12 @@ let mostrar = function() {
     let ceroSeg = (segs < 10 ? '0' : '');
     let ceroMin = (mins < 10 ? '0' : '');
     let ceroHora = (hora < 10 ? '0' : '');
+    let varH = 30 * hora + mins / 2;
 
     $("#digital").html(ceroHora + hora + " : " + ceroMin + mins + " : " + ceroSeg + segs);
+    $('#v-hora').attr('transform', 'rotate(' + varH + " 50 50)");
+    $('#v-min').attr('transform', 'rotate(' + 6 * mins + " 50 50)");
+    $('#v-seg').attr('transform', 'rotate(' + 6 * segs + " 50 50)");
 };
 
 let arrancar = function() {
@@ -37,4 +41,21 @@ let mostrarCrono = function() {
     localStorage.crono = (localStorage.crono || 0);
     localStorage.crono = +localStorage.crono + incCrono; // El signo + convierte a number.
     $("#crono").html((localStorage.crono / divisor).toFixed(cifras) + " s.");
+};
+
+let crearLineas = function() {
+    let svg = document.getElementById("reloj-svg1");
+    let elemento;
+
+    for (let indice = 0; indice < 60; ++indice) {
+        elemento = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+        $(elemento).attr('x1', '50');
+        $(elemento).attr('y1', '2');
+        $(elemento).attr('x2', '50');
+        $(elemento).attr('y2', (indice % 15) == 0 ? '6' : '4');
+        $(elemento).attr('stroke', 'black');
+        $(elemento).attr('stroke-width', (indice % 5) == 0 ? '2' : '1');
+        svg.appendChild(elemento);
+        $(elemento).attr('transform', 'rotate(' + 6 * indice + " 50 50)");
+    }
 };
